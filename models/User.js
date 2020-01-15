f9a1ebf21b5d6f6b65a816ebfc89b2bb1fbd528d
Ryan White <mrwhiterk@gmail.com>
2020-01-15 13:51:19 -0500

    got gen auth working

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const moment = require('moment')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 let UserSchema = new mongoose.Schema(
   {
@@ -23,7 +24,15 @@ let UserSchema = new mongoose.Schema(
     photo: {
       type: String,
       default: ''
-    }
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
   },
   {
     timestamps: true
@@ -31,7 +40,7 @@ let UserSchema = new mongoose.Schema(
 )
 
 UserSchema.pre('save', async function(next) {
-  const user = this;
+  const user = this
   if (user.isModified('password')) {
     let salt = await bcrypt.genSalt(12)
     user.password = await bcrypt.hash(user.password, salt)
@@ -39,10 +48,36 @@ UserSchema.pre('save', async function(next) {
   next()
 })
 
-UserSchema.methods.generateAuthToken = async function () {
-  const user = this;
+UserSchema.methods.generateAuthToken = async function() {
+  const user = this
 
-  const token = jwt.sign({ _id: user._id})
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET_KEY)
+
+  user.tokens = user.tokens.concat({
+    token: token
+  })
+
+  await user.save()
+
+  return token
 }
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.statics.findByCredentials = async (username, password) => {
+  const user = await User.findOne({ username: username })
+
+  if (!user) {
+    throw new Error('wrong email or password')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('wrong email or password')
+  }
+
+  return user
+}
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
 let userController = require('../controllers/user')
+const auth = require('../middleware/auth')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/signup', userController.signup)
+
+router.post('/login', async (req, res) => {
+
+  try {
+    const user = await User.findByCredentials(req.body.username, req.body.password)
+    console.log('user', user)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (err) {
+    res.status(400).send({ error: err})
+  }
+})
 
 module.exports = router;
